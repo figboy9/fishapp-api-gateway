d16c1d0bf21e2a234ef5d23823294500aed7f17d
Katsutoshi Hayashi <ezio1119@gmail.com>
2019-12-23 19:10:59 +0900

    Add: userapiのprotoスキーマと生成コードを追加。grpcクライアントを用意し、UserRepository構造体に埋め込む。

--- src/infrastructure/grpc_client.go
+++ src/infrastructure/grpc_client.go
@@ -5,14 +5,22 @@ import (
 
 	"github.com/ezio1119/fishapp-api-gateway/conf"
 	"github.com/ezio1119/fishapp-api-gateway/domain/post_grpc"
+	"github.com/ezio1119/fishapp-api-gateway/domain/user_grpc"
 	"google.golang.org/grpc"
 )
 
-func NewGrpcClient() post_grpc.PostServiceClient {
+func NewGrpcClient() (post_grpc.PostServiceClient, user_grpc.UserServiceClient) {
 	postConn, err := grpc.Dial(conf.C.Grpc.PostURL, grpc.WithInsecure())
 	if err != nil {
 		log.Fatal(err)
 	}
 	postClient := post_grpc.NewPostServiceClient(postConn)
-	return postClient
+
+	userConn, err := grpc.Dial(conf.C.Grpc.UserURL, grpc.WithInsecure())
+	if err != nil {
+		log.Fatal(err)
+	}
+	userClient := user_grpc.NewUserServiceClient(userConn)
+
+	return postClient, userClient
 }
--- src/interfaces/repository/user_repository.go
+++ src/interfaces/repository/user_repository.go
@@ -1,4 +1,9 @@
 package repository
 
+import (
+	"github.com/ezio1119/fishapp-api-gateway/domain/user_grpc"
+)
+
 type UserRepository struct {
+	Client user_grpc.UserServiceClient
 }
